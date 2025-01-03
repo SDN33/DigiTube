6ba43272c48b073039129807a33a6fc722ce6289
SDN33 <s.deinegri2@gmail.com>
2025-01-03 03:33:04 +0100

    dl

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,19 @@
 // Header Component
-import { Youtube } from 'lucide-react';
+import { Youtube, MessageCircleQuestion } from 'lucide-react';
 import Link from 'next/link';
 
 const Header = () => (
-  <header className="text-center py-16">
+  <header className="text-center py-14">
     <Link href="/" className="flex justify-center mb-6">
       <Youtube className="w-12 h-12 text-red-500 mr-2" />
       <h1 className="text-5xl font-bold">DigiTube</h1>
     </Link>
     <nav>
       <Link href="#about" className="mr-4 hover:underline">À PROPOS</Link>
-      <Link href="/likes" className="mr-4 hover:underline">ACHETER LIKES YOUTUBE</Link>
+      <Link href="/" className="mr-4 hover:underline">VUES YOUTUBE</Link>
+      <Link href="/likes" className="mr-4 hover:underline">LIKES YOUTUBE</Link>
       <Link href="https://digiload.online" className="mr-4 hover:underline">CANVA PRO GRATUIT</Link>
-      <Link href="/contact" className="hover:underline">CONTACT</Link>
+      <Link href="/contact" className="hover:underline group">CONTACT<span className='inline-flex ml-1'><MessageCircleQuestion className='group-hover:rotate-12 transition-transform duration-300' /></span></Link>
     </nav>
   </header>
 );
